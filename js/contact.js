c7d28b80e9b058867343805173fb934ad31b89b1
DeadHedge <drizzt8991@hotmail.com>
2016-06-17 14:15:58 -0400

    Fixed contact.html layout and started to add javascript form functionality

--- js/contact.js
+++ js/contact.js
@@ -3,3 +3,27 @@ function eraseText() {
 	commentsForm.textContent = "";
 	commentsForm.style.color = "black";
 }
+
+function addEvent(el, event, callback) {
+	if ('addEventListener' in el) {
+		el.addEventListener(event, callback, false);
+	} else {
+		el['e' + event + callback] = callback;
+		el[event + callback] = function() {
+			el['e' + event + callback](window.event);
+		};
+		el.attachEvent('on' + event, el[event + callback]);
+	}
+}
+
+(function() {
+		var JSForm = document.getElementById("JSForm");
+		
+		addEvent(form, "submit", function(e) {
+			e.preventDefault();
+			var elements = this.elements;
+			var buttonInput = elements.JSButton.value;
+			var msg = "You input '" + buttonInput + "'. Is this input correct?";
+			document.getElementById("main").textContent = msg;
+		});
+}())
