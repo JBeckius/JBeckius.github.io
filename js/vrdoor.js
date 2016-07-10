9baf6f3ca36fd3e996b62f65bc073ab7b37d587e
John Beckius <jbeckius91@gmail.com>
2016-07-10 11:19:40 -0400

    changed door bevaviour to click event. Doors work now! Also disabled video

--- js/vrdoor.js
+++ js/vrdoor.js
@@ -1,25 +1,32 @@
 var timer;
-var timer2 = window.setInterval(message(2), 2000);
+var timer2;
 
 function message(msg) {
   console.log(msg);
 }
 var door = document.getElementById('door');
-console.log(door.getAttribute('rotation'));
+// console.log(door.getAttribute('rotation'));
 {let i = 0;
-  function doorOpen() {
-    while (door.getAttribute('rotation').y < 90) {
-      door.setAttribute('rotation', "0 " + i + " 0");
-      console.log(door.getAttribute('rotation'));
-      i++;
+  function doorMove() {
+    if (door.getAttribute('rotation').y === 0) {
+      timer = setInterval(function() {
+        if (door.getAttribute('rotation').y < 90) {
+          door.setAttribute('rotation', "0 " + i + " 0");
+          // console.log(door.getAttribute('rotation'));
+          i+= 0.5;
+        } else {clearInterval(timer);}
+      }, 5);
+    } else if(door.getAttribute('rotation').y === 90) {
+      timer = setInterval(function() {
+        if (door.getAttribute('rotation').y > 0) {
+        door.setAttribute('rotation', "0 " + i + " 0");
+        i-=0.5;
+      } else {clearInterval(timer);}
+      // clearInterval(timer2);
+    }, 5);
     }
-  }
-  function doorClose() {
-    while (door.getAttribute('rotation').y > 0) {
-      door.setAttribute('rotation', "0 " + i + " 0");
-      i--;
-    }
+    console.log(door.getAttribute('rotation').y);
   }
 }
-door.addEventListener("mouseenter", timer = window.setInterval(doorOpen, 100000));
-door.addEventListener("mouseleave", timer = window.setInterval(doorClose, 1000));
+door.addEventListener("click", doorMove);
+// door.addEventListener("mouseleave", timer2 = window.setInterval(doorClose, 100));
